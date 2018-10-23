1b0c4bf4beee47d97ea85568d89ec409ce8319a4
Niek <niek@keersop.nl>
2018-10-23 20:26:08 +0200

    initial commit really

--- components/layout/navigation.vue
+++ components/layout/navigation.vue
@@ -1,38 +1,29 @@
 <template>
-  <b-nav class="navbar-toggleable-sm navbar-trans navbar-inverse">
+  <b-navbar toggleable="md" type="dark" variant="info">
     <b-container>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-              data-target="#navbarToggler-1" aria-controls="navbarToggler-1" aria-expanded="false"
-              aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <a class="navbar-brand" href="#">Bootstrap 4 Cover Nav</a>
-      <div class="collapse navbar-collapse pull-xs-right justify-content-end" id="navbarToggler-1">
-        <ul class="navbar-nav mt-2 mt-md-0">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Our Work</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
-          </li>
-          <li class="nav-item">
-            <a class="btn btn-outline-white btn-outline" href="#"><i class="fa fa-user"></i> Login
-            </a>
-          </li>
-        </ul>
-      </div>
+
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <b-navbar-brand href="#">Media portfolio</b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <b-navbar-nav>
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item-dropdown>
+            <template slot="button-content">
+              Portfolio
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Signout</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
     </b-container>
-  </b-nav>
+  </b-navbar>
 </template>
 <script>
   export default {
-    name: "navigation"
+
   }
 </script>
 <style lang="scss">
---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <navigation/>
+    <b-container>
+      <nuxt/>
+    </b-container>
+  </div>
+</template>
+<script>
+  import navigation from "~/components/layout/navigation";
+
+  export default {
+    components: {
+      navigation
+    }
+  }
+</script>
+<style lang="scss">
+  nav {
+    margin-bottom: 5rem;
+  }
+</style>
