489d95d327c9436024ed3aaae77fca799ab249ad
Niek <ik@niekvangogh.nl>
2018-10-24 14:59:03 +0200

    updated page route and name

--- components/layout/navigation.vue
+++ components/layout/navigation.vue
@@ -4,24 +4,21 @@
 
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
       <b-navbar-brand to="/">Media portfolio</b-navbar-brand>
-
       <b-collapse is-nav id="nav_collapse">
-
         <b-navbar-nav>
           <b-nav-item to="/">Home</b-nav-item>
           <b-nav-item-dropdown>
             <template slot="button-content">
               Portfolio
             </template>
-            <b-dropdown-item to="/portfolio/1">Week 1</b-dropdown-item>
-            <b-dropdown-item to="/portfolio/2">Week 2</b-dropdown-item>
-            <b-dropdown-item to="/portfolio/3">Week 3</b-dropdown-item>
-            <b-dropdown-item to="/portfolio/4">Week 4</b-dropdown-item>
-            <b-dropdown-item to="/portfolio/5">Week 5</b-dropdown-item>
-            <b-dropdown-item to="/portfolio/6">Week 6</b-dropdown-item>
-            <b-dropdown-item to="/portfolio/7">Week 7</b-dropdown-item>
-            <b-dropdown-item to="/portfolio/8">Week 8</b-dropdown-item>
-            <b-dropdown-item to="/portfolio/9">Week 9</b-dropdown-item>
+            <b-dropdown-item to="/week/1">Week 1</b-dropdown-item>
+            <b-dropdown-item to="/week/2">Week 2</b-dropdown-item>
+            <b-dropdown-item to="/week/3">Week 3</b-dropdown-item>
+            <b-dropdown-item to="/week/4">Week 4</b-dropdown-item>
+            <b-dropdown-item to="/week/5">Week 5</b-dropdown-item>
+            <b-dropdown-item to="/week/6">Week 6</b-dropdown-item>
+            <b-dropdown-item to="/week/7">Week 7</b-dropdown-item>
+            <b-dropdown-item to="/week/8">Week 8</b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Media portfolio</h1>
-    <p>Dit is mijn media portfolio, op deze website staan de opdrachten waar ik de vorige weken aan heb gewerkt. Klik boven aan de pagina op het knopje 'Opdrachten' om de week te selecteren.</p>
+    <p>Dit is mijn media portfolio, op deze website staan de opdrachten waar ik de vorige weken aan heb gewerkt. Klik
+      boven aan de pagina op het knopje 'Opdrachten' om de week te selecteren.</p>
   </div>
 </template>
