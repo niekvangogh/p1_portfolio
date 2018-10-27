974d0456d9575d88f3e3906fc275953f335cfa9e
Niek <niek@keersop.nl>
2018-10-27 20:17:52 +0200

    added the betekenis page

---
+++ pages/betekenis.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-row>
+    <b-col>
+      <h1>Wat betekend het maken van website voor MIJ?</h1>
+
+      <p>
+        Het maken van deze website vond ik erg leuk, dit is iets wat ik altijd al leuk heb gevonden. Ik heb sinds juni
+        2018 een baan als frontend developer bij een lokaal bedrijf in Eindhoven. Hier werk ik aan een handvol aantal
+        websites.
+      </p>
+      <p>
+        Het maken van websites is zoals ik al zei een favourite bezigheid van mij. Het is iets waar ik mezelf redelijk
+        goed in vind (op het maken van een design na dan ;)). Ik doe dit met veel passie en wil binnenkort als ZZP'er
+        websites maken voor starters, op deze manier kan ik meer ervaring krijgen als developer en mijn LinkedIn profiel
+        (zie footer) meer en beter opbouwen.
+      </p>
+      <p>
+        Met dat gezegd, de reden waarom ik geen media zal kiezen voor de 2e periode van het eerste semester is dat ik
+        niet heel erg creatief ben en omdat ik dus een ZZP ga beginnen waar ik denk dat ik meer aan business zal hebben
+        dan media. Hier kan ik in 10 weken meer ervaring opdoen, waar ik later meer aan zal hebben waarschijnlijk.
+      </p>
+      <p>
+        Als je dit nog leest, en mij een mailtje stuurt naar <a href="mailto:mediaisgeenict@niekvangogh.nl">DIT</a>
+        email adres, krijg je een gratis pizza van mij :)
+      </p>
+    </b-col>
+  </b-row>
+</template>
